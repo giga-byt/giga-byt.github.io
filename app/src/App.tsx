60945fe54147bd4908d1011cab9621829a632b85
mag <bill.tang.03.25@gmail.com>
2023-10-07 17:23:36 -0400

    removed old code

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { TerminalContextProvider } from 'react-terminal';
 import './App.css';
 import FullPage from './Home';
 
 function App() {
   return (
-    <TerminalContextProvider>
       <div className="App">
         <FullPage/>
       </div>
-    </TerminalContextProvider>
   );
 }
 
